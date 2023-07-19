55c36f3729217af74987eca4abd9fe06e8cd4937
jidoil <dotteda@gmail.com>
2023-07-19 15:03:38 +0900

    feat add "server view controller를 추가하였습니다"
    - kiyaapi에서 임시로 server view의 역할을 하던 코드들을 제거하였습니다.
    - server view controller 클래스를 생성하여 역할을 위임하였습니다.

--- python/kiya_api.py
+++ python/kiya_api.py
@@ -11,6 +11,7 @@ __version__ = "0.0.1"
 import gazu
 
 from model.server_connection_model import *
+from python.controllers.server_view_controller import ServerConnetionController
 from validations.properties import *
 from view.server_connection_view_module.server_connection_view import ServerConnectionView
 
@@ -32,31 +33,9 @@ class KiyaAPI:
         self._password = password
 
         gazu.set_host(self._address)
-        gazu.log_in(self._id, self._password)
-
-        self.server_connection_model = ServerConnectionModel()
-        self.server_connection_view = ServerConnectionView()  # 현재 열려있는 프로젝트와 에셋,샷 그리고 활동중인 아티스트의 수를 보여줍니다.
-        self.server_connection_view.show()
-        self.server_connection_model.server_statsChanged.connect(self.update_table)
-        self.server_connection_model.arrange_project_status(self.get_all_open_projects())
-        self.server_connection_view.id_and_passwordRecieved.connect(self.auth_login)
-        self.server_connection_model.login_dataChanged.connect(self.log_in_to_project)
-
-
-    def log_in_to_project(self, valid_login_data: dict) -> None:
-        try:
-            res = gazu.log_in(valid_login_data["id"], valid_login_data["password"])
-            print(f'login: {res["login"]}')
-        except Exception as ex:
-            print(f'{repr(ex)}, exception occured')
-        if res:
-            self.server_connection_view.log_in_succeed()
-
-    def auth_login(self, login_form_data: dict) -> None:
-        self.server_connection_model.auth_login(login_form_data)
-
-    def update_table(self, server_stats):
-        self.server_connection_view.add_row(server_stats)
+        self.gazu = gazu.log_in(self._id, self._password)
+        self.server_connection_controller = ServerConnetionController(self.gazu)
+        self.server_connection_controller.server_connection_model.arrange_project_status(self.get_all_open_projects())
 
     def print_all_assets(self) -> list:
         all_asset_list = gazu.asset.all_assets_for_project(self.project)
